cdd51721ff6653c42801e906b8ec189274aae6b6
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-02-10 14:55:22 +0700

    add config module server in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 import { FormsModule } from "@angular/forms";
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
