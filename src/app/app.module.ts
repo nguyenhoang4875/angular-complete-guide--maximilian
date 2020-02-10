a2b6b064a42fb9acc58ab1a145df689f0df2caf1
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-02-10 16:36:20 +0700

    Assignment solution (add warning and success)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { AppComponent } from "./app.component";
 import { FormsModule } from "@angular/forms";
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlterComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent,WarningAlterComponent, SuccessAlertComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
