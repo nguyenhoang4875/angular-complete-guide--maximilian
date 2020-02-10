f53311ddce292dea0cceba41a39c37b40f28130c
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-02-10 15:21:18 +0700

    add component servers by CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from "./app.component";
 
 import { FormsModule } from "@angular/forms";
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
