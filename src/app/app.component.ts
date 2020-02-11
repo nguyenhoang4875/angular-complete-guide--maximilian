3115c0e885ef14fa802621fb76ca1e9930c38135
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-02-11 11:00:33 +0700

    practicing directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,12 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  name = "my-first-app";
-  username = "";
+  showSecret = false;
+  log =[];
+
+
+  onToggleDetails(){
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length +1);
+  };
 }
