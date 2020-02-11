4ee30aeba7c69c158518fbc00c6ddd37c40e9544
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-02-11 17:15:55 +0700

    create the components shopping and recipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 import { FormsModule } from "@angular/forms";
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RecipesComponent, RecipesListComponent, RecipesDetailComponent, RecipeItemComponent, ShoppingListComponent, ShoppingEditComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
