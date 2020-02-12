b12eadfbaf2cb6c105432338c1bec39dc5056499
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-02-12 16:01:36 +0700

    binding to custom properties

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 import { FormsModule } from "@angular/forms";
+import { ServerElementComponent } from './server-element/server-element.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,  ServerElementComponent, CockpitComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
