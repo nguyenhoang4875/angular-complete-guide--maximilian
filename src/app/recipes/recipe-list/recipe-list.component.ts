e7f91979ea6c219ee9a580ddae7e175a59f49526
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-02-12 09:18:54 +0700

    outputting a list of recipe with ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -12,6 +12,11 @@ export class RecipeListComponent implements OnInit {
       "A Test Recipe",
       "This is simply a test",
       "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/8/6/0/WU2301_Four-Cheese-Pepperoni-Pizzadilla_s4x3.jpg.rend.hgtvcom.826.620.suffix/1565115622965.jpeg"
+    ),
+    new Recipe(
+      "A Test Recipe",
+      "This is simply a test",
+      "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/8/6/0/WU2301_Four-Cheese-Pepperoni-Pizzadilla_s4x3.jpg.rend.hgtvcom.826.620.suffix/1565115622965.jpeg"
     )
   ];
   constructor() {}
