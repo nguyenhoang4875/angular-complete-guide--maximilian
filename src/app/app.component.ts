7792de7345aa750bd6d3bba1fa3603ba75798726
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-02-14 17:20:53 +0700

    directives-start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = "my-first-app";
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
 }
