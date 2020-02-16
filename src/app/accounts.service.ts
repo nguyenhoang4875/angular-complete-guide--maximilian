966f73340b0937cb88912b6da3852b07ba4c6073
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-02-16 11:40:00 +0700

    using Services for Cross-Component communication

--- src/app/accounts.service.ts
+++ src/app/accounts.service.ts
@@ -1,5 +1,5 @@
 import { LoggingService } from './logging.service';
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 @Injectable()
 export class AccountsService {
   accounts = [
@@ -17,7 +17,10 @@ export class AccountsService {
     }
   ];
 
+  statusUpdated = new EventEmitter<string>();
+
   constructor (private loggingService: LoggingService){}
+
 
   addAccount(name: string, status: string) {
     this.accounts.push({ name: name, status: status });
