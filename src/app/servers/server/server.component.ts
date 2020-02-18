5c8a75bd4f8dcd821a78db0ea07d96304c24d888
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-02-18 08:51:36 +0700

    --amend

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from "@angular/core";
 
 import { ServersService } from "../servers.service";
 import { ActivatedRoute, Params, Router } from "@angular/router";
-import { ThrowStmt } from "@angular/compiler";
 
 @Component({
   selector: "app-server",
@@ -20,7 +19,7 @@ export class ServerComponent implements OnInit {
 
   ngOnInit() {
     const id = +this.route.snapshot.params["id"];
-    this.server = this.serversService.getServer(1);
+    this.server = this.serversService.getServer(id);
 
     this.route.params.subscribe((params: Params) => {
       this.server = this.serversService.getServer(+params["id"]);
