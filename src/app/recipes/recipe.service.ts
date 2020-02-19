8ad2953c5405162c259a112ced1e7b87174ecd4d
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-02-19 14:45:37 +0700

    configuring Route Parameters

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -31,4 +31,7 @@ export class RecipeService {
   addIngredientsToShoppingList(ingredients: Ingredient[]){
     this.slService.addIngredients(ingredients);
   }
+  getRecipe(index: number){
+    return this.recipes[index];
+  }
 }
