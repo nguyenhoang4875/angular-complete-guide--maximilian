2dd7fd86cea91c5b2165e5b03d950cbb867b6d81
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-02-26 13:21:58 +0700

    practicing Reactive Forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { ActiveUsersComponent } from './active-users/active-users.component';
-import { InactiveUsersComponent } from './inactive-users/inactive-users.component';
-import { CounterService } from './counter.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ActiveUsersComponent,
-    InactiveUsersComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [CounterService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
