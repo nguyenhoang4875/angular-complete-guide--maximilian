84d905927f21e73514a9e0324137a3ab1853c29b
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-02-27 11:20:39 +0700

    adding a Delete and Clear(Cancel) Funtionality

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Params } from "@angular/router";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 import { FormGroup, FormControl, FormArray, Validators } from "@angular/forms";
 import { RecipeService } from "../recipe.service";
-import { Recipe } from "../recipe.model";
+import { ThrowStmt } from "@angular/compiler";
 
 @Component({
   selector: "app-recipe-edit",
@@ -16,7 +16,8 @@ export class RecipeEditComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private recipeService: RecipeService
+    private recipeService: RecipeService,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -37,9 +38,10 @@ export class RecipeEditComponent implements OnInit {
  */
     if (this.editMode) {
       this.recipeService.updateRecipe(this.id, this.recipeForm.value);
-    } else{
+    } else {
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+    this.onCancel();
   }
   onAddIngredient() {
     (<FormArray>this.recipeForm.get("ingredients")).push(
@@ -51,6 +53,10 @@ export class RecipeEditComponent implements OnInit {
         ])
       })
     );
+  }
+
+  onCancel() {
+    this.router.navigate(["../"], { relativeTo: this.route });
   }
 
   private initForm() {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -43,4 +43,8 @@ export class RecipeService {
     this.recipes[index] = newRecipe;
     this.recipesChange.next(this.recipes.slice());
   }
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChange.next(this.recipes.slice());
+  }
 }
