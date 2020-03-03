abc2455f8b082db1800a14b87822c92eef41393c
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-03-03 14:18:52 +0700

    outputting Posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Post } from "./post.model";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -54,7 +54,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe(posts => {
-        console.log(posts);
+        this.loadedPosts = posts;
       });
   }
 }
