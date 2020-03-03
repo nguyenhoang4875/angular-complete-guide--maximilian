7495e0f15e0f0efbb68296f849589d2a8c96f399
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-03-03 10:41:24 +0700

    Getting data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     this.http
       .post(
-        'https://ng-complete-guide-8888.firebaseio.com/posts.json',
+        "https://ng-complete-guide-8888.firebaseio.com/posts.json",
         postData
       )
       .subscribe(responseData => {
@@ -27,9 +29,18 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get("https://ng-complete-guide-8888.firebaseio.com/posts.json")
+      .subscribe(posts => {
+        console.log(posts);
+      });
   }
 }
