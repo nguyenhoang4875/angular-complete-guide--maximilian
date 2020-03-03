4d00429fc322a0bf18e5f989e09d5700af4248a5
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-03-03 16:35:06 +0700

    error Handling & UX

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,6 +29,7 @@ export class AppComponent implements OnInit, OnDestroy {
         this.loadedPosts = posts;
       },
       error => {
+        this.isFetching = false;
         this.error = error.message;
       }
     );
@@ -48,6 +49,7 @@ export class AppComponent implements OnInit, OnDestroy {
         this.loadedPosts = posts;
       },
       error => {
+        this.isFetching = false;
         this.error = error.message;
         console.log(error);
       }
@@ -60,6 +62,11 @@ export class AppComponent implements OnInit, OnDestroy {
       this.loadedPosts = [];
     });
   }
+
+  onHandleError() {
+    this.error = null;
+  }
+
   ngOnDestroy() {
     this.errorSub.unsubscribe();
   }
