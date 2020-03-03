b481bcf1664ff4c61b3e27e8ed8cf1e72931a3d5
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-03-03 11:16:59 +0700

    using Types with HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
+import { Post } from "./post.model";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -15,10 +16,10 @@ export class AppComponent implements OnInit {
     this.fetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     this.http
-      .post(
+      .post<{ name: string }>(
         "https://ng-complete-guide-8888.firebaseio.com/posts.json",
         postData
       )
@@ -38,10 +39,12 @@ export class AppComponent implements OnInit {
 
   private fetchPosts() {
     this.http
-      .get("https://ng-complete-guide-8888.firebaseio.com/posts.json")
+      .get<{ [key: string]: Post }>(
+        "https://ng-complete-guide-8888.firebaseio.com/posts.json"
+      )
       .pipe(
         map(responseData => {
-          const postsArray = [];
+          const postsArray: Post[] = [];
           for (const key in responseData) {
             if (responseData.hasOwnProperty(key)) {
               postsArray.push({ ...responseData[key], id: key });
