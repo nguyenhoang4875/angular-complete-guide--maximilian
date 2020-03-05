acf86c53eeb455835647f1e76e4c1ca6fba9f234
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-03-05 10:04:00 +0700

    changing the Response Body Type

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -70,7 +70,8 @@ export class PostsService {
   deletePosts() {
     return this.http
       .delete("https://ng-complete-guide-8888.firebaseio.com/posts.json", {
-        observe: "events"
+        observe: "events",
+        responseType: "json"
       })
       .pipe(
         tap(event => {
