6088e313e2a1c76bd82b23e084dec2f1ffd486e6
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-03-08 14:11:28 +0700

    handling Form Input

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,12 +1,18 @@
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { format } from "url";
 
 @Component({
   selector: "app-auth",
   templateUrl: "./auth.component.html"
 })
 export class AuthComponent {
-  isLoginMode:boolean =true;
-  onSwitchMode(){
+  isLoginMode: boolean = true;
+  onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
+  }
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset();
   }
 }
