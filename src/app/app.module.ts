779fb6f4c616a8b17fdcc681d9016c6eb213277e
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-03-13 10:26:12 +0700

    implementing Lazy Loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 import { HeaderComponent } from "./header/header.component";
-import { AuthComponent } from "./auth/auth.component";
 import { AlertComponent } from "./shared/alert/alert.component";
-import { RecipesModule } from "./recipes/recipes.module";
 import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 import { SharedModule } from "./shared/shared.module";
 import { CoreModule } from "./core.module";
@@ -20,7 +18,6 @@ import { AuthModule } from "./auth/auth.module";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    RecipesModule,
     ShoppingListModule,
     AuthModule,
     SharedModule,
