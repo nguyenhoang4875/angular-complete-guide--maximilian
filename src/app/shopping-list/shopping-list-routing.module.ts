7c9162d8ed7038a6c617bcd4858817a7c9b86c5a
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-08-10 14:28:06 +0700

    selecting State

--- src/app/shopping-list/shopping-list-routing.module.ts
+++ src/app/shopping-list/shopping-list-routing.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import { ShoppingListComponent } from "./shopping-list.component";
+import { ShoppingListComponent } from './shopping-list.component';
 
-const routes: Routes = [{ path: "", component: ShoppingListComponent }];
+const routes: Routes = [{ path: '', component: ShoppingListComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class ShoppingListRoutingModule {}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,37 +1,41 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
-import { Ingredient } from "../shared/ingredient.model";
-import { ShoppingListService } from "./shopping-list.service";
-import { Subscription } from "rxjs";
-import { LoggingService } from "../logging.service";
+import { Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
+import { LoggingService } from '../logging.service';
+import { Store } from '@ngrx/store';
 
 @Component({
-  selector: "app-shopping-list",
-  templateUrl: "./shopping-list.component.html",
-  styleUrls: ["./shopping-list.component.css"]
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Ingredient[];
+  ingredients: Observable<{ ingredients: Ingredient[] }>;
   private subscription: Subscription;
   constructor(
     private slService: ShoppingListService,
-    private loggingService: LoggingService
+    private loggingService: LoggingService,
+    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
   ) {}
 
   ngOnInit() {
-    this.ingredients = this.slService.getIngredients();
+    this.ingredients = this.store.select('shoppingList');
+    /*  this.ingredients = this.slService.getIngredients();
     this.subscription = this.slService.ingredientsChanged.subscribe(
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
     );
-
-    this.loggingService.printLog("Hello from ShoppingListComponent ngOnInit");
+  */
+    this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit');
   }
   onEditItem(index: number) {
     this.slService.startedEditing.next(index);
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    // this.subscription.unsubscribe();
   }
 }
